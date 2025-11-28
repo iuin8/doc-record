43878b93fb589f52aed85adaba9894f906cfa60c
Fa <183461750@qq.com>
2025-11-29 03:05:03 +0800

    New translations redis-cluster.sh (English)

--- i18n/en/docusaurus-plugin-content-docs/current/docker/app/devs/data/redis/cluster/test-v2/redis-cluster.sh
+++ i18n/en/docusaurus-plugin-content-docs/current/docker/app/devs/data/redis/cluster/test-v2/redis-cluster.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Defences the service name and network name of the Redis container.
+# Defines the service name and network name of the Redis container
 SERVICE_NAME_PREFIX="data_redis"
 SERVICE_PORT_PREFIX=700
 NETWORK_NAME="midleware"
 
-# build redis-cli orders
+# build redis-cli commands
 CMD="redis-cli --cluster create"
 
 for i in 1 3 4 5 6
 do
     SERVICE_NAME=$SERVICE_NAME_PREFIX$i
-    # Gets a list of IP addresses for Redis containers.
+    # Gets a list of IP addresses for Redis containers
     IP=$(docker service inspect --format='{{range .Endpoint.VirtualIPs}}{{.Addr}}{{end}}' $SERVICE_NAME | sed 's/\/24//g')
     # IP address and port of the Spelling Redis container
     CMD="$CMD $IP:$SERVICE_PORT_PREFIX$i"
@@ -22,5 +22,5 @@ CMD="yes yes | $CMD --cluster-replicas 1 -a foobared"
 
 echo "CMD: $CMD"
 
-# Execute remov-cli command to create Redis Cluster
+# Execute remdis-cli command to create Redis Cluster
 docker run --rm --network $NETWORK_NAME redis:6.0 bash -c "$CMD"
