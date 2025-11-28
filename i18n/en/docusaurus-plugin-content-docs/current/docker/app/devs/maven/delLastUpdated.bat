52c1d096b8e622f953efdff29d56efd674f1c821
Fa <183461750@qq.com>
2025-11-29 03:05:58 +0800

    New translations dellastupdated.bat (English)

--- i18n/en/docusaurus-plugin-content-docs/current/docker/app/devs/maven/delLastUpdated.bat
+++ i18n/en/docusaurus-plugin-content-docs/current/docker/app/devs/maven/delLastUpdated.bat
@@ -1,6 +1,6 @@
 @echo off
 echo @Author Fate
-echo @Date Mon Jan 24 202010:00:56 GMT+0800
+echo @Date Mon Jan 24 20202010:00:56 GMT+0800
 :again
 Set /p REPOSITORY_PATH=Please enter the local your maven warehouse path:
 :: Remove all double quotes
@@ -12,5 +12,5 @@ if not exist "%REPOSITORY_PATH%" (
 for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
    del /s /q "%%i"
 )
-echo Expired files successfully.
+echo Expired files successfully .
 pause;
