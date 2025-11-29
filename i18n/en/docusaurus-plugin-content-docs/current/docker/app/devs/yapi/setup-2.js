4205f11c1244ed2f9859135082fba2adfe96316c
Fa <183461750@qq.com>
2025-11-30 04:39:55 +0800

    New translations setup-2.js (English)

---
+++ i18n/en/docusaurus-plugin-content-docs/current/docker/app/devs/yapi/setup-2.js
@@ -0,0 +1,15 @@
+db.auth('admin', 'admin123456');
+db.createUser({
+    user: 'yapi',
+    pwd: 123456,
+    roles: [
+        {
+            role: 'dbAdmin',
+            db: 'yapi',
+        },
+        {
+            role: 'read write',
+            db: 'yapi',
+        },
+    ],
+});
